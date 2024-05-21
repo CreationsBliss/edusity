65ac818ce0dc99a9b8c106529d5b6192e6b7a2f9
Mac <mac@MusfiQs-MacBook-Air.local>
2024-05-21 09:59:10 +0600

    Program section done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import Hero from "./Components/Hero/Hero";
 import Navbar from "./Components/Navbar/Navbar";
+import Program from "./Components/Program/Program";
+import Title from "./Components/Title/Title";
 
 const App = () => {
   return (
     <div>
       <Navbar></Navbar>
       <Hero></Hero>
+      <div className="container">
+        <Title></Title>
+        <Program></Program>
+      </div>
     </div>
   );
 };
--- src/Components/Program/Program.jsx
+++ src/Components/Program/Program.jsx
@@ -1,17 +1,34 @@
+import program_1 from "../../assets/program-1.png";
+import program_2 from "../../assets/program-2.png";
+import program_3 from "../../assets/program-3.png";
+import program_icon_1 from "../../assets/program-icon-1.png";
+import program_icon_2 from "../../assets/program-icon-2.png";
+import program_icon_3 from "../../assets/program-icon-3.png";
 import "./Program.css";
-import
 
 const Program = () => {
   return (
     <section className="programs">
       <div className="program">
-        <img src="" alt="" />
+        <img src={program_1} alt="program_1" />
+        <div className="caption">
+          <img src={program_icon_1} alt="program_icon_1" />
+          <p>Graduation Degreee</p>
+        </div>
       </div>
       <div className="program">
-        <img src="" alt="" />
+        <img src={program_2} alt="program_2" />
+        <div className="caption">
+          <img src={program_icon_2} alt="program_icon_2" />
+          <p>Masters Degreee</p>
+        </div>
       </div>
       <div className="program">
-        <img src="" alt="" />
+        <img src={program_3} alt="program_3" />
+        <div className="caption">
+          <img src={program_icon_3} alt="program_icon_3" />
+          <p>Post Graduation</p>
+        </div>
       </div>
     </section>
   );
