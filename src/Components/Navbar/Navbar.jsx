578b39301e1f8e8b0320b081b84f1a6c3792f3c8
Mac <mac@MusfiQs-MacBook-Air.local>
2024-05-22 08:01:23 +0600

    Scroll with navbar background change done

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,18 @@
+import { useEffect, useState } from "react";
 import logo from "../../assets/logo.png";
 import "./Navbar.css";
 
 const Navbar = () => {
+  const [menu, setMenu] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      window.scrollY > 50 ? setMenu(true) : setMenu(false);
+    });
+  }, []);
+
   return (
-    <nav className="container">
+    <nav className={`container ${menu ? "main-menu" : " "}`}>
       <img src={logo} alt="logo" />
       <ul>
         <li>Home</li>
