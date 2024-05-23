cb6f8d0916e52d84920e59630905845807a23088
Mac <mac@MusfiQs-MacBook-Air.local>
2024-05-23 10:15:45 +0600

    Smooth scroll done

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,15 +1,15 @@
-import './Footer.css';
+import "./Footer.css";
 
 const Footer = () => {
-    return (
-        <footer className="footer">
-            <p>&copy; 2024 Edusity. All rights reserved.</p>
-            <ul>
-                <li>Terms of Services</li>
-                <li>Privacy Policy</li>
-            </ul>
-        </footer>
-    );
+  return (
+    <footer className="footer">
+      <p>&copy; 2024 Edusity. All rights reserved.</p>
+      <ul>
+        <li>Terms of Services</li>
+        <li>Privacy Policy</li>
+      </ul>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-scroll";
 import logo from "../../assets/logo.png";
 import "./Navbar.css";
 
@@ -15,13 +16,47 @@ const Navbar = () => {
     <nav className={`container ${menu ? "main-menu" : " "}`}>
       <img src={logo} alt="logo" />
       <ul>
-        <li>Home</li>
-        <li>Program</li>
-        <li>About us</li>
-        <li>Campus</li>
-        <li>Testimonials</li>
         <li>
-          <button className="btn">Contact us</button>
+          {" "}
+          <Link to="hero" smooth={true} offset={0} duration={500}>
+            Home
+          </Link>{" "}
+        </li>
+        <li>
+          {" "}
+          <Link to="programs" smooth={true} offset={-260} duration={500}>
+            Program
+          </Link>
+        </li>
+        <li>
+          {" "}
+          <Link to="about" smooth={true} offset={-150} duration={500}>
+            About us
+          </Link>
+        </li>
+        <li>
+          {" "}
+          <Link to="campus" smooth={true} offset={-260} duration={500}>
+            Campus
+          </Link>
+        </li>
+        <li>
+          {" "}
+          <Link to="testimonial" smooth={true} offset={-260} duration={500}>
+            Testimonials
+          </Link>
+        </li>
+        <li>
+          {" "}
+          <Link
+            className="btn"
+            to="contact"
+            smooth={true}
+            offset={-260}
+            duration={500}
+          >
+            Contact us
+          </Link>
         </li>
       </ul>
     </nav>
